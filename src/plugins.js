ad3ee47ac20a0ad47f363f4735e86c80df7e083e
Stig Ofstad <stigofstad@gmail.com>
2023-01-11 11:50:18 +0100

    fixup: remove form-plugin

--- src/plugins.js
+++ src/plugins.js
@@ -1,6 +1,6 @@
 export default [
   import('./plugins/dmt-app'),
-  import('./plugins/form'),
+  // import('@development-framework/form'),
   import('@development-framework/yaml-view'),
   import('@development-framework/tabs'),
   import('@development-framework/blueprint'),
