682df84f5c4d5c203f1db331859e30f1823fdbaa
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-06 14:12:12 +0100

    feat: include explorer plugin and update plugin versions

--- src/plugins.js
+++ src/plugins.js
@@ -1,8 +1,9 @@
 export default [
   import('./plugins/dmt-app'),
-  // import('@development-framework/form'),
+  import('@development-framework/form'),
   import('@development-framework/yaml-view'),
   import('@development-framework/tabs'),
   import('@development-framework/blueprint'),
   import('@development-framework/default-pdf'),
+  import('@development-framework/explorer'),
 ]
